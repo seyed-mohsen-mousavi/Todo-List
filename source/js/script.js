428365be1bb5e1293ad19d77c10cdf280f4acb58
S.mohsen <1mr.rooter3@gmail.com>
2023-10-17 16:59:48 +0330

    update and add comment

--- source/js/script.js
+++ source/js/script.js
@@ -1,35 +1,43 @@
-﻿let ulELm = document.querySelector("ul");
-let inputElm = document.querySelector("input");
-let formElm = document.querySelector(".add");
-let lenthTodo = document.querySelector("p");
+﻿// All variable
+// Mr ROoOt :) 
+let $ = document
+let ulELm = $.querySelector("ul");
+let inputElm = $.querySelector("input");
+let formElm = $.querySelector(".add");
+let lenthTodo = $.querySelector("p");
 formElm.addEventListener("submit", (e) => {
   e.preventDefault();
 });
-let deleteElm = document.querySelectorAll(".delete");
+let deleteElm = $.querySelectorAll(".delete");
 inputElm.addEventListener("keydown", (e) => {
+  // check fild ***
   if (e.key === "Enter") {
     if (
       inputElm.value === "" ||
       inputElm.value === ulELm.lastElementChild.firstElementChild.innerHTML
     ) {
-      document.querySelector(".error").innerHTML = "Error Check Fild";
-      document.querySelector(".error").style.color = "red";
+      // Error style
+      $.querySelector(".error").innerHTML = "Error Check Fild";
+      $.querySelector(".error").style.color = "red";
     } else {
-      document.querySelector(".error").innerHTML = "Added";
-      document.querySelector(".error").style.color = "green";
-      let addLiElm = document.createElement("li");
+      // Add style
+      $.querySelector(".error").innerHTML = "Added";
+      $.querySelector(".error").style.color = "green";
+      // Create New Elment
+      let addLiElm = $.createElement("li");
       addLiElm.setAttribute(
         "class",
         "list-group-item d-flex justify-content-between align-items-center"
       );
-      let addSpanElm = document.createElement("span");
-      let addIElm = document.createElement("i");
+      let addSpanElm = $.createElement("span");
+      let addIElm = $.createElement("i");
       addIElm.setAttribute("class", "fa fa-trash-o delete");
       addLiElm.appendChild(addSpanElm);
       addSpanElm.innerHTML = inputElm.value;
       addLiElm.appendChild(addIElm);
       ulELm.appendChild(addLiElm);
-      deleteElm = document.querySelectorAll(".delete");
+      // add to delete elm 
+      deleteElm = $.querySelectorAll(".delete");
       deleteElm.forEach((e) => {
         e.addEventListener("click", (e) => {
           e.target.parentElement.remove();
